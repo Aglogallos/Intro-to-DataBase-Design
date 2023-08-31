6d8f21ce527f912d695b6ba20cfa4780bf0249f9
Anastasis Aglogallos <79355926+Aglogallos@users.noreply.github.com>
2023-08-31 15:14:24 +0300

    Submitted Version

---
+++ Indexes/indexes_sql.sql
@@ -0,0 +1,13 @@
+﻿
+									--                 INDEXES                  -- 
+
+
+--Φτιάχνουμε το συγκεκριμένο Index ώστε να μειώσουμε το χρόνο που χρειάζεται να αναζητήσουμε τους πελάτες που βρίσκονται μια συγκεκριμένη ώρα στο χώρο
+CREATE INDEX VisitStartDate ON dbo.Visit(VisitStartDate);
+CREATE INDEX VisitEndDate ON dbo.Visit(VisitEndDate);
+
+--Φτιάχνουμε το συγκεκριμένο Index ώστε να μειώσουμε το χρόνο που χρειάζεται να αναζητήσουμε το όνομα του χώρου το οποίο επισκέφθηκε ο πελάτης
+CREATE INDEX PlaceName ON dbo.Places(PlaceName);
+
+--Φτιάχνουμε το συγκεκριμένο Index ώστε να μειώσουμε το χρόνο που χρειάζεται να αναζητήσουμε τo όνομα της υπηρεσίας το οποίο επισκέφθηκε ο πελάτης
+CREATE INDEX ServiceDescription ON dbo.Services(ServiceDescripition);
